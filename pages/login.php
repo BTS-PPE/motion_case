1f0ae777934b2d221a1b4ccee9a4bc60a4a107d2
Brenalax <bry-the-killer@outlook.fr>
2023-03-20 17:55:26 +0100

    New login

--- pages/login.php
+++ pages/login.php
@@ -7,33 +7,54 @@
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="../assets/css/login.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
         <title>Motion Case | Login</title>
     </head>
     <body>
-        <div class="wrapper">
-            <header>Login Form</header>
-            <form action="#">
-            <div class="field email">
-                <div class="input-area">
-                    <input type="text" placeholder="Email Address">
-                    <i class="icon fas fa-envelope"></i>
-                    <i class="error error-icon fas fa-exclamation-circle"></i>
-                </div>
-            <div class="error error-txt">Email can't be blank</div>
-        </div>
-        <div class="field password">
-            <div class="input-area">
-                <input type="password" placeholder="Password">
-                <i class="icon fas fa-lock"></i>
-                <i class="error error-icon fas fa-exclamation-circle"></i>
+        <div class="container" id="container">
+            <div class="form-container sign-up-container">
+                <form action="#">
+                    <h1>Create Account</h1>
+                    <div class="social-container">
+                        <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                        <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                    <span>or use your email for registration</span>
+                    <input type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <button>Sign Up</button>
+                </form>
             </div>
-            <div class="error error-txt">Password can't be blank</div>
-        </div>
-        <div class="pass-txt"><a href="#">Forgot password?</a></div>
-            <input type="submit" value="Login">
-        </form>
-        <div class="sign-txt">Not yet member? <a href="#">Signup now</a></div>
+            <div class="form-container sign-in-container">
+                <form action="#">
+                    <h1>Sign in</h1>
+                    <div class="social-container">
+                        <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                        <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                    <span>or use your account</span>
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <a href="#">Forgot your password?</a>
+                    <button>Sign In</button>
+                </form>
+            </div>
+            <div class="overlay-container">
+                <div class="overlay">
+                    <div class="overlay-panel overlay-left">
+                        <h1>Welcome Back!</h1>
+                        <p>To keep connected with us please login with your personal info</p>
+                        <button class="ghost" id="signIn">Sign In</button>
+                    </div>
+                    <div class="overlay-panel overlay-right">
+                        <h1>Hello, Friend!</h1>
+                        <p>Enter your personal details and start journey with us</p>
+                        <button class="ghost" id="signUp">Sign Up</button>
+                    </div>
+                </div>
+            </div>
         </div>
         <script src="../assets/js/login.js"></script>
     </body>
