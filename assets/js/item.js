51016b5a78f74343915d0f90bb2bd28a80e5ca84
Brenalax <bry-the-killer@outlook.fr>
2023-04-25 17:12:05 +0200

    Update

--- assets/js/item.js
+++ assets/js/item.js
@@ -1,9 +1,13 @@
-var ctrl = document.getElementsByClassName("images")[0];
+var divImages = document.getElementsByClassName("images")[0];
 var mainImage = document.getElementById("mainImage");
 
-ctrl.addEventListener("click", function(e) {
-    var img = e.target;
-    mainImage.src = img.src;
-    mainImage.animate([ { transform: 'scale(1.2)' } ], { duration: 500, fill: 'both' });
-    // $(mainImage).css("transform", "scale(1.2)");
+divImages.addEventListener("click", function(e) {
+    if (e.target.id == "mainImage") {
+        return;
+    } else {
+        var img = e.target;
+        mainImage.src = img.src;
+        mainImage.animate([ { transform: 'scale(1.2)' } ], { duration: 500, fill: 'both' });
+        // $(mainImage).css("transform", "scale(1.2)");
+    }
 });
